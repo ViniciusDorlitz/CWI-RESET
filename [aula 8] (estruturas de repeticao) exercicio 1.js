e7e7432197aa5397d92a31e982c18e6ace832fdb
Vinicius R Dorlitz <64929582+ViniciusDorlitz@users.noreply.github.com>
2021-08-20 21:51:43 -0300

    Add files via upload

---
+++ [aula 8] (estruturas de repeticao) exercicio 1.js
@@ -0,0 +1,21 @@
+/*Exercícios:
+Lembre-se que, para obter o tamanho de um array, é usada a expressão
+ nomeDoArray.length. Ex.:
+
+timesDeFutebol = ["Grêmio", "Inter", "Corinthians", "Flamengo"]
+
+quantidadeDeTimes = timesDeFutebol.length
+
+console.log(quantidadeDeTimes)
+
+1-) Crie um array de 5 posições com quaisquer valores. Utilize qualquer
+ estrutura de repetição apresentada no vídeo para exibir todos os valores do array;*/
+
+
+ artesMarciais = ["kickboxing", "jiu-jitsu", "muay-thai", "karate", "judo"]
+
+ quantidadeDeArtesMarciais = artesMarciais.length
+
+ console.log(quantidadeDeArtesMarciais)
+
+ console.log(artesMarciais)
