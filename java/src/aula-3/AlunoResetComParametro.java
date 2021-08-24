d309570f5b563044af532686110cfa253cf2fc1d
Vinicius Dorlitz <viniciusdorlitz@gmail.com>
2021-08-24 20:05:03 -0300

    update

---
+++ java/src/aula-3/AlunoResetComParametro.java
@@ -0,0 +1,5 @@
+class AlunoResetComParametro{
+    public static void main(String[]args){
+        System.out.println("Sou aluno do Level 1 do CWI RESET! Me chamo " + args[0]);
+    }
+}
---
+++ java/src/aula2/AlunoReset.java
@@ -0,0 +1,6 @@
+class AlunoReset{
+    public static void main (String[]args){
+        System.out.println("Sou aluno do Level 1 do CWI Reset!");
+    }
+}
+
